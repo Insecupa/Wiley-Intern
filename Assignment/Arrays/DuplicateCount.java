b34d4637875a5356138ddccef4d0667145a11c41
Insecupa <65500167+Insecupa@users.noreply.github.com>
2022-09-26 00:25:59 +0530

    Delete DuplicateCount.java

--- Assignment/Arrays/DuplicateCount.java
+++
@@ -1,9 +0,0 @@
-package Day13.Assignment.Arrays;
-
-public class DuplicateCount {
-    public static void main(String[] args) {
-        int[] arr = new int[]{11, 2, 6, 23, 7, 12, 0, 3, 7};
-
-//        System.out.println();
-    }
-}
